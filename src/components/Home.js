c2ac86d1e902f1de536f2f7deb524be8856261ff
rang <jr0503@naver.com>
2025-01-22 09:44:38 +0900

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import "./Home.css";
+import "./reset.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div></div>
+      </header>
+
+      <main>
+        <ul>
+          <li>여성</li>
+          <li>남성</li>
+          <li>키즈</li>
+          <li>반려동물</li>
+          <li>EVENT</li>
+        </ul>
+      </main>
+
+      <footer>Copyright ⓒ CLOTHESºC. All Rights Reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
