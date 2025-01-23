7ea0ac03582d82c825389d37b47175ceceb552cc
rang <jr0503@naver.com>
2025-01-23 17:40:46 +0900

    일단은 최종

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,13 +9,33 @@ function App() {
       </header>
 
       <main>
-        <ul>
-          <li>여성</li>
-          <li>남성</li>
-          <li>키즈</li>
-          <li>반려동물</li>
-          <li>EVENT</li>
+        <nav>
+          <ul id="menu">
+            <li>여성</li>
+            <li></li>
+            <li>남성</li>
+            <li></li>
+            <li>키즈</li>
+            <li></li>
+            <li>반려동물</li>
+            <li></li>
+            <li>EVENT</li>
+          </ul>
+
+          <ul id="icon">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+
+        <div id="vanner"></div>
+
+        <ul className="title">
+          <li></li>
+          <li>오늘 뭐 입지?</li>
         </ul>
+        <div></div>
       </main>
 
       <footer>
