9067620a588099b2f2ca972b2b53962be1f4b82e
rang <jr0503@naver.com>
2025-01-24 18:01:42 +0900

    링크투까진 함

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import "../styles/Login.css";
+import "../styles/reset.css";
+
+function LoginPage() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <nav>
+          <ul id="menu">
+            <li>여성</li>
+            <li></li>
+            <li>남성</li>
+            <li></li>
+            <li>키즈</li>
+            <li></li>
+            <li>반려동물</li>
+            <li></li>
+            <li>EVENT</li>
+          </ul>
+
+          <ul id="icon">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default LoginPage;
