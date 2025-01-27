a28bde01b1a25bfc9d421be9bb78881fb408c809
rangrang-53 <jr0503@naver.com>
2025-01-27 12:36:20 +0900

    로그인, 회원가입 페이지 수정

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Router, Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import login_icon from "../assets/icons/User_alt_light.png";
 import logo from "../assets/images/change.png";
 import "../styles/Login.css";
 import "../styles/reset.css";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <header>
-        <Link to="/"><img src={logo} alt="로고"/>
+        <Link to="/">
+          <img src={logo} alt="로고" />
         </Link>
       </header>
 
@@ -28,16 +30,48 @@ const LoginPage = () => {
           </ul>
 
           <ul id="icon">
-            <li><Link to="/login">
-            <img src={login_icon}></img></Link></li>
+            <li>
+              <Link to="/login">
+                <img src={login_icon}></img>
+              </Link>
+            </li>
             <li></li>
             <li></li>
           </ul>
         </nav>
+        <div id="loginPage_container">
+          <div id="lp_login_container" onClick={() => navigate("/login")}>
+            <p>LOGIN</p>
+            <div className="box">
+              <input type="text" placeholder="아이디"></input>
+              <input type="text" placeholder="비밀번호"></input>
+            </div>
+            <div className="buttons">
+              <button>취소</button>
+              <input type="submit" value={"확인"}></input>
+            </div>
+          </div>
+
+          <div id="lp_signup_container" onClick={() => navigate("/signup")}>
+            <p>SIGNUP</p>
+            <div className="box">
+              <input type="text" placeholder="아이디"></input>
+              <input type="text" placeholder="비밀번호"></input>
+              <input type="text" placeholder="비밀번호 확인"></input>
+              <input type="text" placeholder="닉네임"></input>
+            </div>
+            <div className="buttons">
+              <button>취소</button>
+              <input type="submit" value={"확인"}></input>
+            </div>
+          </div>
+        </div>
       </main>
-      <footer></footer>
+      <footer>
+        <div id="text">Copyright ⓒ CLOTHESºC. All Rights Reserved.</div>
+      </footer>
     </>
   );
-}
+};
 
 export default LoginPage;
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,10 +1,20 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import login_icon from "../assets/icons/User_alt_light.png";
+import logo from "../assets/images/change.png";
 import "../styles/Signup.css";
 import "../styles/reset.css";
 
-function SignupPage() {
+const SignupPage = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <header></header>
+      <header>
+        <Link to="/">
+          <img src={logo} alt="로고" />
+        </Link>
+      </header>
+
       <main>
         <nav>
           <ul id="menu">
@@ -20,15 +30,49 @@ function SignupPage() {
           </ul>
 
           <ul id="icon">
-            <li></li>
+            <li>
+              <Link to="/login">
+                <img src={login_icon}></img>
+              </Link>
+            </li>
             <li></li>
             <li></li>
           </ul>
         </nav>
+
+        <div id="signupPage_container">
+          <div id="sp_login_container" onClick={() => navigate("/login")}>
+            <p>LOGIN</p>
+            <div className="box">
+              <input type="text" placeholder="아이디"></input>
+              <input type="text" placeholder="비밀번호"></input>
+            </div>
+            <div className="buttons">
+              <button>취소</button>
+              <input type="submit" value={"확인"}></input>
+            </div>
+          </div>
+
+          <div id="sp_signup_container" onClick={() => navigate("/signup")}>
+            <p>SIGNUP</p>
+            <div className="box">
+              <input type="text" placeholder="아이디"></input>
+              <input type="text" placeholder="비밀번호"></input>
+              <input type="text" placeholder="비밀번호 확인"></input>
+              <input type="text" placeholder="닉네임"></input>
+            </div>
+            <div className="buttons">
+              <button>취소</button>
+              <input type="submit" value={"확인"}></input>
+            </div>
+          </div>
+        </div>
       </main>
-      <footer></footer>
+      <footer>
+        <div id="text">Copyright ⓒ CLOTHESºC. All Rights Reserved.</div>
+      </footer>
     </>
   );
-}
+};
 
 export default SignupPage;
