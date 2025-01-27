4fc17e65d2acfdb94d7c8466ee1ed683efed31e6
rangrang-53 <jr0503@naver.com>
2025-01-27 12:38:12 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
 import LoginPage from "./pages/LoginPage.js";
+import SignupPage from "./pages/SignupPage.js";
 import "./styles/App.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage/>}></Route>
-      <Route path="/login" element={<LoginPage/>}></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/signup" element={<SignupPage />}></Route>
     </Routes>
   );
 }
